46145ecd72f8ccfd3b6bce1a7098742ee1e0d0a2
Muffin <madlimander@gmail.com>
2019-02-13 18:07:34 -0600

    made function better

--- js/bodyshape.js
+++ js/bodyshape.js
@@ -1,10 +1,12 @@
 var femaleImage = new Image()
 femaleImage.src = "img/female.svg"
 
-function drawFemale() {
-    console.log("draw female")
+var maleImage = new Image()
+maleImage.src = "img/male.svg"
+
+function drawBodyShape(image) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(femaleImage, 0, 0, 300, 500)
+    ctx.drawImage(image, 0, 0, 300, 500)
 }
 
 
@@ -12,9 +14,9 @@ var radios = document.forms["formA"].elements["myradio"];
 for (var r of radios){
     r.onclick = function() {
         if(this.value ==="A"){
-            drawFemale()
+            drawBodyShape(femaleImage)
         } else {
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
+           drawBodyShape(maleImage)
         }
         
 

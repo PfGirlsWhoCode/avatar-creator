439558f2344d5b31dffa01286315dda33acc2b28
Phantom Fire <aklenke123533@outlook.com>
2019-02-20 18:42:05 -0600

    Changed how it draws.

--- js/bodyshape.js
+++ js/bodyshape.js
@@ -13,6 +13,29 @@ maleImage.src = "img/male.svg"
 var maleImage = new Image()
 maleImage.src = "img/male.svg"<*/
 
+var shape = "Triangle"
+
+function drawAvatar() {
+    if(shape ==="Triangle"){
+        drawBodyShape(femaleImage)
+    } 
+    else if(shape === "Pear"){
+        drawBodyShape(maleImage)
+    } 
+    else if(shape === "Circle"){
+        drawBodyShape(femaleImage)
+    }
+    else if (shape ==="Rectangle"){
+        drawBodyShape(maleImage)
+    }
+    else if (shape === "Hourglass"){
+        drawBodyShape(femaleImage)
+    }
+    else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+}
+
 function drawBodyShape(image) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(image, 0, 0, 300, 500)
@@ -23,6 +46,9 @@ var radios = document.forms["formA"].elements["myradio"];
 for (var r of radios){
     r.onclick = function() {
 
+        shape = this.value
+        drawAvatar()
+        /*
         if(this.value ==="Triangle"){
             drawBodyShape(femaleImage)
         } 
@@ -41,5 +67,6 @@ for (var r of radios){
         else {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
         }
+        */
     }
 }

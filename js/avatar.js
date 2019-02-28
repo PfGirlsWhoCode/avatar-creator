bfdf29b524b2683bd62229b9eadbf650f559345e
Natalia <Nataliaelyssarios@gmail.com>
2019-02-27 18:03:30 -0600

    added male and female files brotein shake

--- js/avatar.js
+++ js/avatar.js
@@ -1,18 +1,50 @@
+//Male avatar body types
+var maleApple = new Image()
+maleApple.src = "img/male_apple.svg"
+
+var maleRectangle = new Image()
+maleRectangle.src = "img/male_rectangle.svg"
+
+var maleHourglass = new Image()
+maleHourglass.src = "img/male_hourglass.svg"
+
+var malePear = new Image()
+malePear.src = "img/male_pear.svg"
+
+var maleTriangle = new Image()
+maleTriangle.src = "img/male_triangle.svg"
+
+// Female avatar body types
+var femaleApple = new Image()
+femaleApple.src = "img/female_apple.svg"
+
+var femaleRectangle = new Image()
+femaleRectangle.src = "img/female_rectangle.svg"
+
+var femaleHourglass = new Image()
+femaleHourglass.src = "img/female_hourglass.svg"
+
+var femalePear = new Image()
+femalePear.src = "img/female_pear.svg"
+
+var femaleTriangle = new Image()
+femaleTriangle.src = "img/female_triangle.svg"
+
 function drawAvatar() {
     if(shape ==="Triangle"){
-        drawBodyShape(femaleImage)
+        drawBodyShape(femaleTriangle)
     } 
     else if(shape === "Pear"){
-        drawBodyShape(maleImage)
+        drawBodyShape(femalePear)
     } 
     else if(shape === "Circle"){
-        drawBodyShape(femaleImage)
+        drawBodyShape(femaleCircle)
     }
     else if (shape ==="Rectangle"){
-        drawBodyShape(maleImage)
+        drawBodyShape(femaleRectangle)
     }
     else if (shape === "Hourglass"){
-        drawBodyShape(femaleImage)
+        drawBodyShape(femaleHourglass)
     }
     else {
         ctx.clearRect(0, 0, canvas.width, canvas.height)

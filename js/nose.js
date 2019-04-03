f70cffa4d7469d401d408330cbf50aadbeea3183
Kariden <karideni@icloud.com>
2019-04-03 18:44:21 -0500

    Fixed some unwanted code in the nose department

--- js/nose.js
+++ js/nose.js
@@ -15,12 +15,3 @@ for (var i = 0; i < radios.length; i++) {
 
 
 
-var eye = ""
-
-var radios = document.forms["formeyes"].elements["myradio"];
-for (var i = 0; i < radios.length; i++) { 
-    radios[i].onclick = function() {
-        eye = this.value
-        drawAvatar()
-    }
-}

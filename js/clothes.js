2c891299da16c24707bd4143ce2fa35325346385
Phantom Fire <aklenke123533@outlook.com>
2019-04-10 18:16:32 -0500

    PARTY

--- js/clothes.js
+++ js/clothes.js
@@ -3,8 +3,8 @@ var clothes = ""
 var radios = document.forms["formclothing"].elements["myradio"];
 for (var i = 0; i < radios.length; i++) { 
     radios[i].onclick = function() {
-        console.log(this.value)
         clothes = this.value
+        console.log(clothes)
         drawAvatar()
     }
 }

efd67f60f9c9ba114171c52f03d2d7ca40dbdc56
Muffin <madlimander@gmail.com>
2019-05-15 21:33:29 -0500

    tshirtjeans pear styfffffffffff

--- js/clothes.js
+++ js/clothes.js
@@ -10,18 +10,30 @@ for (var i = 0; i < radios.length; i++) {
 
 var dressHourglass = new Image()
 dressHourglass.src = "img/female_hourglass_dress.svg"
+
 var dressApple = new Image()
 dressApple.src = "img/female_apple_dress.svg"
+
 var dressRectangle = new Image()
 dressRectangle.src = "img/female_rectangle_dress.svg"
+
 var athleticApple = new Image()
 athleticApple.src = "img/female_apple_athletic_wear.svg"
+
 var athleticTriangle = new Image()
 athleticTriangle.src = "img/female_Triangle_athletic_wear.svg"
+
 var maleDressHourglass = new Image()
 maleDressHourglass.src = "img/male_hourglass_dress.svg"
+
 var maleTriangleDress= new Image()
 maleTriangleDress.src = "img/male_triangle_dress.svg"
+
+var femalePearDress = new Image()
+femalePearDress.src = "img/female_pear_dress.svg"
+
+var femalePearTshirt = new Image()
+femalePearTshirt.src = "female_pear_tshirtjeans.svg"
 
 var clothing = {
     "Female": {
@@ -41,8 +53,8 @@ var clothing = {
             "clothingswim": null,
         },
         "Pear": {
-
-
+            "clothingdress" : femalePearDress,
+            "clothingtshirtjeans" : femalePearTshirt,
 
 
 
